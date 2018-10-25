5e7b7db089427e4f3db607d6316b70d77273e5cc
Will Kaczmarek <kaczmarekwill@gmail.com>
2018-10-25 18:56:57 -0400

    Added additional comments

--- mnist_loader.py
+++ mnist_loader.py
@@ -18,13 +18,25 @@ def load_data_wrapper():
     """Based on ``load_data``, but the format is more convenient 
 	for use in our implementation of neural networks."""
     tr_d, va_d, te_d = load_data()
+
+	# Reshape the input element of each training image into a 784-dimensional vector
     training_inputs = [cp.reshape(x, (784, 1)) for x in tr_d[0]]
+	# Reshape the label element of each training image into a 10-dimensional 
+	# vector with the correct label set to 1
     training_results = [vectorized_result(y) for y in tr_d[1]]
+	# Zip inputs and results in to 2-tuples
     training_data = zip(training_inputs, training_results)
+
+	# Reshape validation inputs in the same way as training inputs
     validation_inputs = [cp.reshape(x, (784, 1)) for x in va_d[0]]
+	# Zip inputs and results in to 2-tuples. Note labels are still integers
     validation_data = zip(validation_inputs, va_d[1])
+
+	# Reshape test inputs in the same way as training images
     test_inputs = [cp.reshape(x, (784, 1)) for x in te_d[0]]
+	# Zip inputs and results in to 2-tuples. Again, labels are still integers
     test_data = zip(test_inputs, te_d[1])
+
     return (training_data, validation_data, test_data)
 
 def vectorized_result(j):
